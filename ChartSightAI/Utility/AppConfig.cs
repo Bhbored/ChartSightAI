0d2a9ea5e943302cbe8e2d1e2380b11c367fb857
Bhbored <bhbored2022@gmail.com>
2025-10-26 12:51:33 +0200

    added login flow

---
+++ ChartSightAI/Utility/AppConfig.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChartSightAI.Utility
+{
+    public static class AppConfig
+    {
+        public const string SupabaseUrl = "YOUR_SUPABASE_URL";
+        public const string SupabaseAnonKey = "YOUR_SUPABASE_KEY";
+    }
+}
